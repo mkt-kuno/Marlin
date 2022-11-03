37c996c2085db63bb5115d91f6f320e5eca194ac
Kuno Makoto <mkt-kuno@iis.u-tokyo.ac.jp>
2022-11-03 14:00:19 +0900

    加速度調整/TMCハイブリッド速度調整
    (cherry picked from commit b3544a4a100a462526e550101146644209572e17)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3109,10 +3109,10 @@
    */
   #define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     100  // [mm/s]
-  #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     100
-  #define Y2_HYBRID_THRESHOLD    100
+  #define X_HYBRID_THRESHOLD     120  // [mm/s]
+  #define X2_HYBRID_THRESHOLD    120
+  #define Y_HYBRID_THRESHOLD     120
+  #define Y2_HYBRID_THRESHOLD    120
   #define Z_HYBRID_THRESHOLD       6
   #define Z2_HYBRID_THRESHOLD      6
   #define Z3_HYBRID_THRESHOLD      3
